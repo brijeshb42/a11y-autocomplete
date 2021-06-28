a2a417824f6a2af17543837e5c712946bfbea670
Brijesh Bittu <brijeshb42@gmail.com>
2021-06-28 14:24:13 +0530

    Working component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import classNames from 'classnames';
+
 import './App.css';
 
 function App() {
   const [suggestions, setSuggestions] = React.useState([]);
+  const [userInput, setUserInput] = React.useState('');
+  const [hoveredItemIndex, setHoveredItemIndex] = React.useState(-1);
+  const [selectedItem, setSelectedItem] = React.useState(null);
+  const [showSuggestions, setShowSuggestions] = React.useState(false);
 
   function fetchSuggestions(query) {
     const urlParams = new URLSearchParams();
@@ -13,17 +19,47 @@ function App() {
   }
 
   function handleInputChange(ev) {
+    setUserInput(ev.target.value);
     fetchSuggestions(ev.target.value);
   }
 
+  function handleItemSelect(index) {
+    const selectedItem = suggestions[index];
+    if (!items) {
+      return;
+    }
+    setSelectedItem(selectedItem);
+    setShowSuggestions(false);
+  }
+
+  React.useEffect(() => setUserInput(selectedItem?.name ?? ''), [selectedItem]);
+
   return (
     <div className="ac__container">
-      <input type="text" className="ac__input" onChange={handleInputChange} />
-      {!!suggestions.length && (
-        <ul className="suggestion-list">
-          {suggestions.map((suggestion) => {
+      <input
+        type="text"
+        className="ac__input"
+        value={userInput}
+        onChange={handleInputChange}
+        onFocus={() => setShowSuggestions(true)}
+      />
+      {!!suggestions.length && showSuggestions && (
+        <ul
+          className="suggestion-list"
+          onMouseOut={() => setHoveredItemIndex(-1)}
+        >
+          {suggestions.map((suggestion, index) => {
             return (
-              <li key={suggestion.name} className="suggestion-list-item">
+              <li
+                key={suggestion.name}
+                className={classNames('suggestion-list-item', {
+                  'suggestion-list-item--hovered': hoveredItemIndex === index,
+                  'suggestion-list-item--selected':
+                    selectedItem?.name === suggestion.name,
+                })}
+                onMouseOver={() => setHoveredItemIndex(index)}
+                onClick={() => handleItemSelect(index)}
+              >
                 {suggestion.name}
               </li>
             );
