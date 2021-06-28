8aa395e528d4b5f2852fb02ef6a73df44fcf8867
Brijesh Bittu <brijeshb42@gmail.com>
2021-06-28 12:15:28 +0530

    Update to fetch and render suggestions on type

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,34 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const [suggestions, setSuggestions] = React.useState([]);
+
+  function fetchSuggestions(query) {
+    const urlParams = new URLSearchParams();
+    urlParams.append('query', query);
+    fetch(`https://autocomplete.clearbit.com/v1/companies/suggest?${urlParams}`)
+      .then((resp) => resp.json())
+      .then((newSuggestions) => setSuggestions(newSuggestions));
+  }
+
+  function handleInputChange(ev) {
+    fetchSuggestions(ev.target.value);
+  }
+
   return (
     <div className="ac__container">
-      <input type="text" className="ac__input" />
-      <ul className="suggestion-list">
-        <li className="suggestion-list-item">Suggestion 1</li>
-        <li className="suggestion-list-item suggestion-list-item--selected">
-          Suggestion 2
-        </li>
-        <li className="suggestion-list-item">Suggestion 3</li>
-        <li className="suggestion-list-item">Suggestion 4</li>
-        <li className="suggestion-list-item suggestion-list-item--hovered">
-          Suggestion 5
-        </li>
-        <li className="suggestion-list-item">Suggestion 6</li>
-        <li className="suggestion-list-item">Suggestion 7</li>
-        <li className="suggestion-list-item">Suggestion 8</li>
-        <li className="suggestion-list-item">Suggestion 9</li>
-        <li className="suggestion-list-item">Suggestion 10</li>
-      </ul>
+      <input type="text" className="ac__input" onChange={handleInputChange} />
+      {!!suggestions.length && (
+        <ul className="suggestion-list">
+          {suggestions.map((suggestion) => {
+            return (
+              <li key={suggestion.name} className="suggestion-list-item">
+                {suggestion.name}
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
